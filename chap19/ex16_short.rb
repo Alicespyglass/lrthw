24e7ea8937810566e64e25788d3ee83b3eccc200
alicespyglass <alicespyglass@gmail.com>
2017-02-20 17:36:09 +0000

    Ex16: Reading and Writing Files

---
+++ chap19/ex16_short.rb
@@ -0,0 +1,29 @@
+filename = ARGV.first
+# instructions. CTRL-C quits it
+puts "We're going to erase #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+# accept input?
+$stdin.gets
+# opens file. w for write mode 
+puts "Opening the file..."
+target = open(filename, 'w')
+# emptying file to nothing
+puts "Truncating the file. Goodbye!"
+target.truncate(0)
+
+puts "Now I'm going to ask you for three lines."
+#Asking for input 
+print "line 1: "
+line1 = $stdin.gets.chomp
+print "line 2: "
+line2 = $stdin.gets.chomp
+print "line 3: "
+line3 = $stdin.gets.chomp
+
+puts "I'm going to write these to the file."
+
+target.write("line1, "\n" line2, "\n", line3")
+
+puts "And finally, we close it."
+target.close
