9430f7b6bdf92dace7fef6649b1e9091aa4d2636
alicespyglass <alicespyglass@gmail.com>
2017-02-20 16:18:23 +0000

    Ex10: What Was That?

---
+++ chap13/ex10_examples.rb
@@ -0,0 +1,22 @@
+backslash = "\\kitty"
+puts backslash
+bell = "1. \\a, BEL, bell (sound), \akitty"
+puts bell
+backspace = "2. \\b, BS, backspace, \bkitty"
+puts backspace
+formfeed = "3. \\f, FF, formfeed (printing page break), \fkitty"
+puts formfeed
+linefeed = "4. \\n, LF, linefeed (new line), \nkitty"
+puts linefeed
+carriage_return = "5. \\r, CR, Carriage Return (return to beginning, over-writes thought), \rkitty, \\r"
+puts carriage_return
+horizontal_tab = "6. \\t, TAB, horizontal tab, \tkitty"
+puts horizontal_tab
+hex = "7. \\uxxxx, Character with 16-bit hex value xxxx (Unicode only), no example"
+puts hex
+vertical_tab = "7. \\v, VT, vertical tab, \vkitty"
+puts vertical_tab
+octal_value = "8. \\ooo, character with octal value ooo, no example"
+puts octal_value
+hex_value = "9. \\xhh, chararcter with hex value hh, no example"
+puts hex_value
