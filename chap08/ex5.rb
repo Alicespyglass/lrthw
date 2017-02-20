1f824ab99608a11127d39f5c9ae3207095a50889
alicespyglass <alicespyglass@gmail.com>
2017-02-20 15:42:46 +0000

    Ex5: More Variables and Printing

---
+++ chap08/ex5.rb
@@ -0,0 +1,21 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Lets talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+height_cm = 2.54*height
+weight_kg = 2.2*weight
+puts "His height in centimeters is #{height_cm}."
+puts "His weight in kilograms is #{weight_kg}."
