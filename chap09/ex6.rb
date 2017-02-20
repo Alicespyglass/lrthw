7697705660e801f3c1aa69dba6f5a2a171d8bbf6
alicespyglass <alicespyglass@gmail.com>
2017-02-20 15:48:50 +0000

    Ex6: Strings and Text

---
+++ chap09/ex6.rb
@@ -0,0 +1,26 @@
+# defining a variabale called types of people. Input is number
+types_of_people = 10
+# defining a variable called x which is a string, including output of another variable
+x = "There are #{types_of_people} types of people."
+# string variable 
+binary = "binary"
+# string variable shortening "do not"
+do_not = "don't"
+# string with two other variables 
+y = "Those who know #{binary} and those who #{do_not}."
+# printing x and y
+puts x
+puts y
+# printing a string of two strings
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+# variable with boolean
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+# print variable
+puts joke_evaluation
+# string variable
+w = "This is the left side of..."
+e = "a string with a right side."
+# addition of two string variables
+puts w + e
